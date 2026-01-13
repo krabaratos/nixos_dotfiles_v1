92de34603a672cdcb90af3e615e236e7a93ba537
Krabaratos <63770336+krabaratos@users.noreply.github.com>
2026-01-13 13:45:02 +0530

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,139 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_IN";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  #Flatpak
+  services.flatpak.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.manish = {
+    isNormalUser = true;
+    description = "Manish Meena";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+      kdePackages.kdialog
+      kdePackages.flatpak-kcm
+    ];
+  };
+  
+  #Gaming
+  programs.steam.dedicatedServer.openFirewall = true;
+  programs.steam.remotePlay.openFirewall = true;
+
+  #GPU Drivers
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+     mesa 
+     vulkan-loader 
+     vulkan-tools 
+     vulkan-validation-layers
+    ];
+  };
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+ htop
+ dpkg
+ uwufetch
+ discord 
+ vscode
+ darkly
+ firefox
+ gamescope
+ gamemode
+ mangohud
+ goverlay
+ heroic-unwrapped
+ steam
+ steam-run
+ appimage-run
+ prismlauncher-unwrapped
+ flatpak-xdg-utils
+ libportal-qt6
+ jdk21_headless
+ javaPackages.compiler.openjdk21
+ jre21_minimal
+ kdePackages.kpipewire
+ ];
+
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
