302a02ab7662e1266bbbd126d8fa58464b219336
Krabaratos <63770336+krabaratos@users.noreply.github.com>
2026-01-13 02:33:23 -0800

    Update gaming.nix

--- gaming.nix
+++ gaming.nix
@@ -7,8 +7,6 @@
   };
 
   config = {
-    programs.gamemode.enable = true; # for performance mode
-
     programs.steam = {
       enable = true; # install steam
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
@@ -16,18 +14,7 @@
     };
 
     environment.systemPackages = with pkgs; [
-      heroic # install heroic launcher
-      lutris # install lutris launcher
-      mumble # install voice-chat
-      protonup-qt # GUI for installing custom Proton versions like GE_Proton
-      (retroarch.override {
-        cores = with libretro; [ # decide what emulators you want to include
-          puae # Amiga 500
-          scummvm
-          dosbox
-        ];
-      })
-      teamspeak_client # install voice-chat
-    ];
+
+];
   };
 }
